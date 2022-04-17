703a70ab9ea94b4c6c3f7148e9aed74f0f668773
Khine Kyaw Tun <khinekyaw.ai@gmail.com>
2022-04-17 21:40:39 +0630

    Add new font, button and input etc.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,29 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     fontFamily: {
-      sans: ['"Open Sans"', 'sans-serif'],
+      sans: ['"Mada"', "sans-serif"]
     },
     extend: {
       fontFamily: {
-        'cookie': ['Cookie', 'sans-serif'],
+        mada: ["Mada", "sans-serif"],
+        berkshireswash: ["Berkshire Swash", "sans-serif"]
+      },
+      colors: {
+        primary: {
+          DEFAULT: "#E3332D",
+          50: "#F9D0CF",
+          100: "#F6BFBD",
+          200: "#F19C99",
+          300: "#ED7975",
+          400: "#E85651",
+          500: "#E3332D",
+          600: "#BF1F19",
+          700: "#8D1713",
+          800: "#5C0F0C",
+          900: "#2A0706"
+        }
       }
-    },
+    }
   },
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-  ],
+  plugins: [require("tailwindcss"), require("autoprefixer")]
 }
